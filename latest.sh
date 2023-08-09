9cc948f2673c3f10090250e405acae54ee11cf07
praley10 <praley10@gmail.com>
2023-08-08 18:54:43 -0600

    Create latest.sh

---
+++ latest.sh
@@ -0,0 +1,20 @@
+#### This is not a Secure File ####
+
+#!/bin/bash
+
+#Let's stop the script if anything errors out. 
+set -o errexit
+apt update
+apt upgrade -y
+#### Install script dependencies ####
+# Install Git
+apt install git -y
+# Install Curl Command
+apt install curl -y
+#### Install Qemu ####
+sudo apt -y install qemu-guest-agent
+sudo systemctl enable qemu-guest-agent
+sudo systemctl start qemu-guest-agent
+#### Post Install Clean up ####
+cd
+rm latest.sh
