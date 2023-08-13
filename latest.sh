77e7ceec9e28e170ec95b731dafcdc908f807cfd
praley10 <praley10@gmail.com>
2023-08-13 17:36:12 -0600

    Update latest.sh

--- latest.sh
+++ latest.sh
@@ -4,14 +4,14 @@
 set -o errexit
 apt update
 apt upgrade -y
-#### Install script dependencies ####
-# Install Git
+####---- Install script dependencies ----####
+####---- Install Git ----####
 apt install git -y
-# Install Curl Command
+####---- Install Curl Command ----####
 apt install curl -y
-#### Install Qemu ####
+####----- Install Qemu ----####
 sudo apt -y install qemu-guest-agent
 sudo systemctl enable qemu-guest-agent
 sudo systemctl start qemu-guest-agent
-#### Post Install Clean up ####
+####---- Post Install Clean up ----####
 rm latest.sh
